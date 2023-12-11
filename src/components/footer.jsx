2b77cf103879a5f6aa2d7b47606aa7213bc6b217
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-11 15:07:03 +0100

    correction footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,13 +3,13 @@ import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => (
   <div className="footer">
-    <a href="https://www.github.com/yourusername" target="_blank" rel="noreferrer" className="social-icon">
+    <a href="https://github.com/DanielDSCosta" target="_blank" rel="noreferrer" className="social-icon">
       <FaGithub size={40} />
     </a>
-    <a href="https://www.twitter.com/yourusername" target="_blank" rel="noreferrer" className="social-icon">
+    <a href="https://twitter.com/Arwasoff" target="_blank" rel="noreferrer" className="social-icon">
       <FaTwitter size={40} />
     </a>
-    <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noreferrer" className="social-icon">
+    <a href="https://www.linkedin.com/in/daniel-da-silva-costa-b0b0a3171/" target="_blank" rel="noreferrer" className="social-icon">
       <FaLinkedin size={40} />
     </a>
   </div>
