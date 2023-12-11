28706d31c4bb7992d9761ce0d4656a72f98031ab
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-11 17:10:32 +0100

    page About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import projects from '../data/projects.json';
 
 
-const About = () => (
-    <div>
-      <h2>Blog</h2>
-      <p>Bienvenue sur mon blog. Partagez vos pensées et expériences ici.</p>
+const About = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    // Préchargement des images
+    projects.forEach(project => {
+      const img = new Image();
+      img.src = project.largeImage;
+    });
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        setSelectedImage(null);
+      }
+    };
+    
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  return (
+    <div className="about">
+      <h1 className="about-title">About Me</h1>
+      <p className="about-description">
+      Line 1. <br/>
+        Line 2. <br/>
+       Line 3. <br/>
+      </p>
+      <h2 className="projects-title">Mes Projets</h2>
+      <div className="card-container">
+        {projects.sort((a, b) => a.id - b.id).map((item, index) => (
+          <div className="card" key={index} onClick={() => setSelectedImage(item)}>
+            <h3 className="card-title">{item.title}</h3>
+            <img src={item.image} alt={item.github} className="card-image" />
+          </div>
+        ))}
+      </div>
+      {selectedImage && (
+        <div className="modal" onClick={() => setSelectedImage(null)}>
+          <div className="modal-content">
+          <h2 className="modal-title">{selectedImage.title}</h2>
+          <img src={selectedImage.largeImage} alt={selectedImage.github} className="modal-image" onClick={(e) => e.stopPropagation()} />
+           <p className="modal-description">{selectedImage.tags.join(' , ')}</p>
+         <a href={selectedImage.github} target="_blank" rel="noopener noreferrer" className="modal-link">Voir sur GitHub</a>
+        </div>
+        </div>
+      )}
     </div>
   );
-  export default About;
+};
+
+export default About;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,15 @@
-
+import React from 'react';
 
 const Home = () => (
-    <div>
-      <h2>Accueil</h2>
-      <p>Bienvenue sur mon portfolio !</p>
-    </div>
-  );
+  <div className="home">
+    <p className="home-intro">Portfolio</p>
+    <h1 className="home-title">
+      Da Silva Costa<br />
+      Daniel
+    </h1>
+    <p className="home-description">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum fringilla augue vitae tincidunt. Curabitur vitae tortor id libero.
+    </p>
+  </div>
+);
   export default Home;
