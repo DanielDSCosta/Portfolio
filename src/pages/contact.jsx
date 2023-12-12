6e15933d15b1d68321ed42bd21230270c7f76b73
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-12 14:46:51 +0100

    test

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import Home from './home';
+
 
 
 const Contact = () => {
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Votre message a été envoyé !');
+    event.target.submit();
+  };
   return (
   <div className="about">
     <h2 className="about-title">Contactez moi</h2>
-    <form className="form" action="https://formsubmit.co/daniel.dasilva.gsi@gmail.com" method="POST" enctype="multipart/form-data">
+    <form className="form" action="https://formsubmit.co/daniel.dasilva.gsi@gmail.com" method="POST" enctype="multipart/form-data" onSubmit={handleSubmit}>
       <div className="form-group">
         <label className="form-label" for="email">Votre Email:</label>
         <input className="form-input" type="email" id="email" name="email" required />
@@ -21,11 +25,11 @@ const Contact = () => {
         <textarea className="form-textarea" id="body" name="body" required />
       </div>
       <input className="form-submit" type="submit" value="Envoyer" />
-      <input type="hidden" name="_next" value={Home}></input>
+      <input type="hidden" name="_next" value="https://danieldasilvacosta.netlify.app/" ></input>
       <input type="hidden" name="_captcha" value="false"></input>
     </form>
   </div>
 );
-  }
+  };
 
 export default Contact;
