b068faf3fb78f3641970ae6cecc238ff5505222d
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-12 16:14:54 +0100

    parfait

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 const Contact = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -10,26 +8,23 @@ const Contact = () => {
   };
   return (
   <div className="about">
-    <h2 className="about-title">Contactez moi</h2>
+    <h2 className="about-title">Contactez-moi</h2>
     <form className="form" action="https://formsubmit.co/daniel.dasilva.gsi@gmail.com" method="POST" enctype="multipart/form-data" onSubmit={handleSubmit}>
       <div className="form-group">
-        <label className="form-label" for="email">Votre Email:</label>
-        <input className="form-input" type="email" id="email" name="email" required />
+        <input className="form-input" type="email" id="email" name="email" required placeholder="Votre Email:" />
       </div>
       <div className="form-group">
-        <label className="form-label" for="subject">Objet:</label>
-        <input className="form-input" type="text" id="subject" name="subject" required />
+        <input className="form-input" type="text" id="subject" name="subject" required placeholder="Objet:" />
       </div>
       <div className="form-group">
-        <label className="form-label" for="body">Message:</label>
-        <textarea className="form-textarea" id="body" name="body" required />
+        <textarea className="form-textarea" id="body" name="body" required placeholder="Message:" />
       </div>
       <input className="form-submit" type="submit" value="Envoyer" />
       <input type="hidden" name="_next" value="https://danieldasilvacosta.netlify.app/" ></input>
       <input type="hidden" name="_captcha" value="false"></input>
     </form>
   </div>
-);
-  };
+  );
+};
 
 export default Contact;
