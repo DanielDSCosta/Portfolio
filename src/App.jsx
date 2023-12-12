d5494e224ad41126099359294c43b7843783ba77
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-12 12:11:10 +0100

    suppression page contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/home';
 import Project from './pages/project';
-import Contact from './pages/contact';
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Header from './components/header';
@@ -24,7 +23,6 @@ const App = () => (
           <Route exact path="/" Component={Home} />
           <Route path="/project" Component={Project} />
           <Route path="/about" Component={About} />
-          <Route path="/contact" Component={Contact} />
         </Routes>
       </div>
       <Footer/>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,6 @@ const Header = () => (
       <li><NavLink to="/">Accueil</NavLink></li>
       <li><NavLink to="/project">Projets</NavLink></li>
       <li><NavLink to="/about">About Me</NavLink></li>
-      <li><NavLink to="/contact">Contact</NavLink></li>
     </ul>
   </nav>
 );
