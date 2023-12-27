546bd81d1329efe697747bf7447e9ea31967a63d
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-27 15:54:54 +0100

    correction

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <nav>
     <ul className="nav">
-      <li><NavLink to="/">Accueil</NavLink></li>
-      <li><NavLink to="/project">Projets</NavLink></li>
-      <li><NavLink to="/about">About Me</NavLink></li>
-      <li><NavLink to="/contact">Contact</NavLink></li>
+      <li className="nav-item"><NavLink to="/">Accueil</NavLink></li>
+      <li className="nav-item"><NavLink to="/project">Projets</NavLink></li>
+      <li className="nav-item"><NavLink to="/about">About Me</NavLink></li>
+      <li className="nav-item"><NavLink to="/contact">Contact</NavLink></li>
     </ul>
   </nav>
 );
